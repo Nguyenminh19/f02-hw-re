bdb01a3a24f523e4cb9f6d4ffca5cf69ef356d12
Nguyenminh19 <minh6513@gmail.com>
2025-03-06 14:18:50 +0700

    resolve hw0304 problem

---
+++ src/_03_operators_conditions/HW04.java
@@ -0,0 +1,15 @@
+package _03_operators_conditions;
+
+public class HW04 {
+    public static void main(String[] args) {
+        int a = 2, b = 10;
+
+        if (a > b) {
+            System.out.printf("%d lon hon %d", a, b);
+        } else if (a < b) {
+            System.out.printf("\n %d nho hon %d", a, b);
+        } else {
+            System.out.println("Hai so bang nhau");
+        }
+    }
+}
