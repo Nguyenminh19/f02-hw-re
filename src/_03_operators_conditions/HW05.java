fbf96a7d534adaf08c610bbb9f1795e2e12a6c82
Nguyenminh19 <minh6513@gmail.com>
2025-03-06 14:20:17 +0700

    resolve hw0305 problem

---
+++ src/_03_operators_conditions/HW05.java
@@ -0,0 +1,9 @@
+package _03_operators_conditions;
+
+public class HW05 {
+    public static void main(String[] args) {
+        int length = 5, width = 10;
+        System.out.println("Perimeter: " + (length + width) * 2);
+        System.out.println("Area: " + length * width);
+    }
+}
